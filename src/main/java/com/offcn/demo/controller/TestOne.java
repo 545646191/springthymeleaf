d0694c44fd6831e2b497e02487a98f5da3a2535c
HPH <545646191@qq.com>
2020-11-05 21:41:55 +0800

    新增类——常量

--- src/main/java/com/offcn/demo/controller/TestOne.java
+++ src/main/java/com/offcn/demo/controller/TestOne.java
@@ -1,5 +1,13 @@
 package com.offcn.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestOne {
     static final int One = 90;
+@GetMapping("/getOne")
+    public void getOne(){
+    System.out.println("1");
+}
 }
